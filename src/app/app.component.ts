6462407fe65a8d551543589e8eed03421e0190e4
fpaci2 <Dedalo$25>
2021-07-09 11:22:26 +0200

    refactorizzato codice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   points: any = []
   frames: any = []
   actualFrame = 0
-  actualThrow = 1
+  actualThrow = 0
 
   ngOnInit() {
     this.points = this.generateArrayOfNumbers(11)
@@ -23,7 +23,7 @@ export class AppComponent {
   createSets() {
     this.frames = []
     for (let i = 0; i < 10; i++) {
-      this.frames.push({ number: i + 1, throw1: null, throw2: null, throw3: null, frameScore: null, spare: false, strike: false })
+      this.frames.push({ score: [], totalScore: null })
     }
   }
 
@@ -32,92 +32,104 @@ export class AppComponent {
   }
 
   addPoint(point: number) {
-    this.frames[this.actualFrame].strike = (this.actualThrow === 1 && point === 10) || (this.actualFrame === 9 && (point === 10 || this.frames[this.actualFrame].strike))
-    this.frames[this.actualFrame].spare = this.actualThrow === 2 && (this.frames[this.actualFrame].throw1 || 0) + point === 10
-
-    if (this.actualFrame > 0) {
-      if (this.frames[this.actualFrame - 1].frameScore < 30) {
-        if (this.frames[this.actualFrame - 1].spare) {
-          this.frames[this.actualFrame - 1].frameScore += this.actualThrow === 1 ? point : 0
-        } else if (this.frames[this.actualFrame - 1].strike) {
-          this.frames[this.actualFrame - 1].frameScore += point
-          if (this.actualFrame > 1) {
-            if (this.frames[this.actualFrame - 2].strike && this.frames[this.actualFrame - 2].frameScore < 30) {
-              this.frames[this.actualFrame - 2].frameScore += point
+    let actualFrame = this.actualFrame
+    let actualThrow = this.actualThrow
+    if (this.actualFrame === 9) {
+      if (this.actualThrow === 0) {
+        if (point === 10) {
+          this.frames[this.actualFrame].score.push({ point: 10, view: 'X' })
+          this.actualThrow += 1
+          this.points = this.generateArrayOfNumbers(11)
+        } else {
+          this.frames[this.actualFrame].score.push({ point: point, view: point })
+          this.actualThrow += 1
+          this.points = this.generateArrayOfNumbers(11 - point)
+        }
+      } else {
+        if (point === 10 && (this.actualThrow === 0 || this.actualThrow === 2 ||
+          (this.actualThrow === 1 && this.frames[this.actualFrame].score[0].point === 10))) {
+          this.frames[this.actualFrame].score.push({ point: 10, view: 'X' })
+        } else if (this.actualThrow === 1 && point + this.frames[this.actualFrame].score[0].point === 10) {
+          this.frames[this.actualFrame].score.push({ point: point, view: '/' })
+        } else {
+          this.frames[this.actualFrame].score.push({ point: point, view: point })
+        }
+        if (this.actualThrow === 0 || (this.actualThrow === 1 &&
+          (this.frames[this.actualFrame].score[0].view === 'X' ||
+          this.frames[this.actualFrame].score[1].view === '/'))) {
+          this.actualThrow += 1
+          this.points = this.generateArrayOfNumbers(11)
+        } else {
+          this.points = []
+          this.frames[this.actualFrame].totalScore = this.sumScore(this.frames[this.actualFrame].score)
+        }
+      }
+    } else {
+      if (this.actualThrow === 0) {
+        if (point === 10) {
+          this.frames[this.actualFrame].score.push({ point: 10, view: null })
+          this.frames[this.actualFrame].score.push({ point: 0, view: 'X' })
+          this.frames[this.actualFrame].totalScore = this.sumScore(this.frames[this.actualFrame].score)
+          this.actualFrame += 1
+          this.points = this.generateArrayOfNumbers(11)
+        } else {
+          this.frames[this.actualFrame].score.push({ point: point, view: point })
+          this.actualThrow += 1
+          this.points = this.generateArrayOfNumbers(11 - point)
+        }
+      } else if (this.actualThrow === 1) {
+        if (point + this.frames[this.actualFrame].score[0].point === 10) {
+          this.frames[this.actualFrame].score.push({ point: point, view: '/' })
+        } else {
+          this.frames[this.actualFrame].score.push({ point: point, view: point })
+        }
+        this.frames[this.actualFrame].totalScore = this.sumScore(this.frames[this.actualFrame].score)
+        this.actualFrame += 1
+        this.actualThrow = 0
+        this.points = this.generateArrayOfNumbers(11)
+      }
+    }
+    if (actualFrame > 0) {
+      if (this.sumScore(this.frames[actualFrame - 1].score) < 30) {
+        if (this.frames[actualFrame - 1].score[1].view === '/' && actualThrow === 0) {
+          this.frames[actualFrame - 1].score.push({ point: point, view: point })
+          this.frames[actualFrame - 1].totalScore = this.sumScore(this.frames[actualFrame - 1].score)
+        } else if (this.frames[actualFrame - 1].score[1].view === 'X') {
+          this.frames[actualFrame - 1].score.push({ point: point, view: point })
+          this.frames[actualFrame - 1].totalScore = this.sumScore(this.frames[actualFrame - 1].score)
+          if (actualFrame > 1) {
+            if (this.frames[actualFrame - 2].score[1].view === 'X' &&
+              this.sumScore(this.frames[actualFrame - 2].score) < 30) {
+              this.frames[actualFrame - 2].score.push({ point: point, view: point })
+              this.frames[actualFrame - 2].totalScore = this.sumScore(this.frames[actualFrame - 2].score)
             }
           }
         }
       }
     }
-    if (this.actualFrame === 9) {
-      this.frames[this.actualFrame]['throw' + this.actualThrow] = point
-      let terzoTiro = this.frames[this.actualFrame].strike || this.frames[this.actualFrame].spare
-      if (this.actualThrow < 2 || (terzoTiro && this.actualThrow < 3)) {
-        if (this.actualThrow === 1 && point === 10) {
-          this.points = this.generateArrayOfNumbers(11)
-        } else if (this.actualThrow === 2 && (this.frames[this.actualFrame].spare || point === 10)) {
-          this.points = this.generateArrayOfNumbers(11)
-        } else {
-          this.points = this.generateArrayOfNumbers(11 - point)
-        }
-        this.actualThrow += 1
-      } else {
-        this.frames[this.actualFrame].frameScore = (this.frames[this.actualFrame].throw1 || 0) + (this.frames[this.actualFrame].throw2 || 0) + (this.frames[this.actualFrame].throw3 || 0)
-        this.points = []
-      }
-    } else if (this.actualThrow === 1 && this.remainigPoint(point)) {
-      this.frames[this.actualFrame].throw1 = point
-      this.actualThrow = 2
-      this.points = this.generateArrayOfNumbers(11 - point)
-    } else {
-      this.frames[this.actualFrame].throw2 = point
-      this.frames[this.actualFrame].frameScore = (this.frames[this.actualFrame].throw1 || 0) + point
-      this.actualThrow = 1
-      this.actualFrame += 1
-      this.points = this.generateArrayOfNumbers(11)
-    }
-  }
-
-  remainigPoint(point: number) {
-    return 10 - point
   }
 
   getTotalScore(index: number) {
-    if (this.frames[index].frameScore) {
+    if (this.frames[index].totalScore) {
       let totalScore = 0
       for (let i = 0; i <= index; i++) {
-        totalScore += this.frames[i].frameScore || 0
+        totalScore += this.frames[i].totalScore
       }
       return totalScore
     }
   }
 
-  getSecondScoreSimbol(frame: any) {
-    if (frame.throw2 === 10 && frame.throw1 === null) {
-      return 'X'
-    } else if (frame.throw1 + frame.throw2 === 10) {
-      return '/'
+  sumScore(score: any) {
+    let total = 0
+    for (let i = 0; i < score.length; i++) {
+      total += score[i].point || 0
     }
-    return frame.throw2
-  }
-
-  getSimbolLastFrame(frame: any, position: number) {
-    let point = frame['throw' + position]
-    if (point) {
-      if ((position === 1 && point === 10) || frame.throw3 === 10) {
-        return 'X'
-      } else if (position === 2 && point === 10 && frame.throw1 === 10) {
-        return 'X'
-      } else if (position === 2 && point + frame.throw1 === 10) {
-        return '/'
-      }
-    }
-    return point
+    return total
   }
 
   refreshData() {
     this.actualFrame = 0
-    this.actualThrow = 1
+    this.actualThrow = 0
     this.createSets()
     this.points = this.generateArrayOfNumbers(11)
   }
